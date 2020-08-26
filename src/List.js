f2c7438954f00eb11cb938fe7c142499663471cb
simo54 <sorzesimone1@gmail.com>
2020-08-26 15:47:14 +0200

    Implemented play bar button

--- src/List.js
+++ src/List.js
@@ -1,18 +1,18 @@
 import React from "react";
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PlayArrowSharpIcon from "@material-ui/icons/PlayArrowSharp";
+import PauseSharpIcon from "@material-ui/icons/PauseSharp";
 
-function List({key, artist, title, image, duration, del}) {
-
-    return (
-        <div>
-        <PlayArrowIcon />
-        <p>{artist}</p>
-        <p>{title}</p>
-        <img src={image} width="150px" alt="Album Cover"/>
-        <p>{duration}</p>
-        </div>
-    )
+function List({ key, artist, title, image, duration, del }) {
+  const [toggledRowPlay, setTogglerRowPlay] = React.useState(false);
+  return (
+    <div>
+      <div onClick={() => setTogglerRowPlay(!toggledRowPlay)}>{toggledRowPlay ? <PauseSharpIcon /> : <PlayArrowSharpIcon />}</div>
+      <p>{artist}</p>
+      <p>{title}</p>
+      <img src={image} width='150px' alt='Album Cover' />
+      <p>{duration}</p>
+    </div>
+  );
 }
-
 
 export default List;
